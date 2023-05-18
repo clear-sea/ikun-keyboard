1488d4d8a769f062156baf7a705578bf55f758f4
dsy4567 <dsy4567@outlook.com>
2023-05-18 22:45:05 +0800

    为注释添加空格，改善强迫症患者阅读代码的体验

--- ikun_keyboard.py
+++ ikun_keyboard.py
@@ -1,9 +1,9 @@
-from pynput import keyboard#首先导入模块
+from pynput import keyboard # 首先导入模块
 import playsound
 
 def press(key):
     try:
-        #print(key.char)#调试输出
+        # print(key.char) # 调试输出
 
         if key.char=="j":
             playsound.playsound("J.mp3")
@@ -16,7 +16,7 @@ def press(key):
 
     except AttributeError:
         if key==keyboard.Key.ctrl or key==keyboard.Key.ctrl_l or key==keyboard.Key.ctrl_r:
-            #print("只因你太美")
+            # print("只因你太美")
             playsound.playsound("ctrl.mp3")
 
 with keyboard.Listener(on_press=press) as listener:

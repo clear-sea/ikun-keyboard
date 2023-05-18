42b69f5aecee252aef56f54b661c0d3a61d2f1c6
dsy4567 <dsy4567@outlook.com>
2023-05-18 22:49:53 +0800

    添加更多空格

--- ikun_keyboard.py
+++ ikun_keyboard.py
@@ -5,19 +5,19 @@ def press(key):
     try:
         # print(key.char) # 调试输出
 
-        if key.char=="j":
+        if key.char == "j":
             playsound.playsound("J.mp3")
-        elif key.char=="n":
+        elif key.char == "n":
             playsound.playsound("N.mp3")
-        elif key.char=="t":
+        elif key.char == "t":
             playsound.playsound("T.mp3")
-        elif key.char=="m":
+        elif key.char == "m":
             playsound.playsound("M.mp3")
 
     except AttributeError:
-        if key==keyboard.Key.ctrl or key==keyboard.Key.ctrl_l or key==keyboard.Key.ctrl_r:
+        if key == keyboard.Key.ctrl or key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
             # print("只因你太美")
             playsound.playsound("ctrl.mp3")
 
-with keyboard.Listener(on_press=press) as listener:
+with keyboard.Listener(on_press = press) as listener:
     listener.join()
